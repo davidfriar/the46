c62041844be90465d20b5e102d980efbbf2ce54c
David Friar <david.friar@cognifide.com>
2025-03-02 20:05:58 +0000

    old changes

--- ergogen/build.sh
+++ ergogen/build.sh
@@ -1,7 +1,18 @@
 ergogen -d --clean . 
 
-cp output/pcbs/left_pcb.kicad_pcb ../pcbs/left/left.kicad_pcb
-cp output/pcbs/right_pcb.kicad_pcb ../pcbs/right/right.kicad_pcb
+
+sed '/segment/, $w ../pcbs/left/traces-left.tmp' ../pcbs/left/left.kicad_pcb
+sed '/segment/, $w ../pcbs/right/traces-right.tmp' ../pcbs/right/right.kicad_pcb
+
+# cp output/pcbs/left_pcb.kicad_pcb ../pcbs/left/left.kicad_pcb
+# cp output/pcbs/right_pcb.kicad_pcb ../pcbs/right/right.kicad_pcb
+
+head -n -3 output/pcbs/left_pcb.kicad_pcb > ../pcbs/left/left.kicad_pcb
+cat ../pcbs/left/traces-left.tmp >> ../pcbs/left/left.kicad_pcb
+
+head -n -3 output/pcbs/right_pcb.kicad_pcb > ../pcbs/right/right.kicad_pcb
+cat ../pcbs/right/traces-right.tmp >> ../pcbs/right/right.kicad_pcb
+
 
 kicad-cli pcb export step -f --subst-models -o ../case/pcbs/left_pcb.step    ../pcbs/left/left.kicad_pcb
 kicad-cli pcb export step -f --subst-models -o ../case/pcbs/right_pcb.step    ../pcbs/right/right.kicad_pcb
